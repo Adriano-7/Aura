0c82a34af2e9632b598cfda804a641e3bba232d6
Adriano Machado <93844395+Adriano-7@users.noreply.github.com>
2023-10-20 13:59:18 +0100

    Updated create.sql

--- database/create.sql
+++ database/create.sql
@@ -1,184 +1,160 @@
 -- SCHEMA: lbaw2384
-SET search_path TO lbaw2384;
+SET search_path TO lbaw2324;
 
-DROP TABLE IF EXISTS DenunciaComentario;
-DROP TABLE IF EXISTS DenunciaEvento;
-DROP TABLE IF EXISTS motivosDenuncia;
-DROP TABLE IF EXISTS Denuncia;
-DROP TABLE IF EXISTS TagEvento;
-DROP TABLE IF EXISTS Tag;
-DROP TABLE IF EXISTS Evento;
-DROP TABLE IF EXISTS ClienteEvento;
-DROP TABLE IF EXISTS ClienteOrganizacao;
-DROP TABLE IF EXISTS Organizacao;
-DROP TABLE IF EXISTS PedidoRegisto;
-DROP TABLE IF EXISTS Ficheiro;
-DROP TABLE IF EXISTS VotoComentario;
-DROP TABLE IF EXISTS Comentario;
-DROP TABLE IF EXISTS NotificacaoEvento;
-DROP TABLE IF EXISTS NotificacaoUtilizador;
-DROP TABLE IF EXISTS Notificacao;
-DROP TABLE IF EXISTS Administrador;
-DROP TABLE IF EXISTS Cliente;
-DROP TABLE IF EXISTS Utilizador;
-
+DROP TABLE IF EXISTS Utilizador CASCADE;
 CREATE TABLE Utilizador (
-    idUtilizador SERIAL PRIMARY KEY,
-    nome VARCHAR(256)  NOT NULL,
-    email VARCHAR(256) UNIQUE NOT NULL,
-    password text NOT NULL,
-    foto text
+    idUtilizador UUID PRIMARY KEY,
+    nome TEXT  NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    foto TEXT
 );
 
-CREATE TABLE Organizacao (
-    idOrganizacao SERIAL PRIMARY KEY,
-    nome VARCHAR(255),
-    descricao TEXT,
-    aprovada BOOLEAN
-);
-
-CREATE TABLE Administrador (
-    idUtilizador INTEGER PRIMARY KEY REFERENCES Utilizador (idUtilizador) ON UPDATE CASCADE ON DELETE CASCADE
-);
-
+DROP TABLE IF EXISTS Cliente CASCADE;
 CREATE TABLE Cliente (
-    idUtilizador INTEGER PRIMARY KEY REFERENCES Utilizador (idUtilizador) ON UPDATE CASCADE ON DELETE CASCADE
+    idUtilizador UUID PRIMARY KEY REFERENCES Utilizador (idUtilizador) ON DELETE CASCADE
 );
 
-
-CREATE TABLE Notificacao (
-    idNotificacao SERIAL PRIMARY KEY,
-    texto TEXT,
-    data TIMESTAMP NOT NULL CHECK (data <= now()),
-    url VARCHAR(255) NOT NULL,
-    visto BOOLEAN NOT NULL DEFAULT FALSE,
-    utilizadorNotificado INTEGER NOT NULL REFERENCES Utilizador (idUtilizador) ON UPDATE CASCADE ON DELETE CASCADE
+DROP TABLE IF EXISTS Administrador CASCADE;
+CREATE TABLE Administrador (
+    idUtilizador UUID PRIMARY KEY REFERENCES Utilizador (idUtilizador) ON DELETE CASCADE
 );
 
-
-
-CREATE TABLE NotificacaoEvento (
-    idNotificacao INT,
-    idUtilizador INT NOT NULL,
-    FOREIGN KEY (idNotificacao) REFERENCES Notificacao (idNotificacao),
-    FOREIGN KEY (idUtilizador) REFERENCES Utilizador (idUtilizador)
-);
-
-
-
-
-CREATE TABLE PedidoRegisto (
-    idPedidoRegisto SERIAL PRIMARY KEY,
-    data DATE,
-    idUtilizador INT NOT NULL,
-    idOrganizacao INT NOT NULL,
-    FOREIGN KEY (idUtilizador) REFERENCES Cliente (idUtilizador),
-    FOREIGN KEY (idOrganizacao) REFERENCES Organizacao (idOrganizacao)
-);
-
-CREATE TABLE Evento (
-    idEvento SERIAL PRIMARY KEY,
-    nome VARCHAR(255),
+DROP TABLE IF EXISTS Organizacao CASCADE;
+CREATE TABLE Organizacao (
+    idOrganizacao UUID PRIMARY KEY,
+    nome TEXT NOT NULL,
     descricao TEXT,
-    localizacao VARCHAR(255),
-    dataInicio DATE,
-    dataFim DATE,
-    isPublic BOOLEAN,
-    idOrganizacao INT NOT NULL,
-    FOREIGN KEY (idOrganizacao) REFERENCES Organizacao (idOrganizacao)
+    foto TEXT,
+    aprovada BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE Comentario (
-    idComentario SERIAL PRIMARY KEY,
-    idUtilizador INT NOT NULL,
-    texto TEXT,
-    data DATE,
-    balancoVotos INT,
-    idEvento INT NOT NULL,
-    FOREIGN KEY (idUtilizador) REFERENCES Cliente (idUtilizador),
-    FOREIGN KEY (idEvento) REFERENCES Evento (idEvento)
+DROP TABLE IF EXISTS Evento CASCADE;
+CREATE TABLE Evento (
+    idEvento UUID PRIMARY KEY,
+    nome TEXT NOT NULL,
+    descricao TEXT,
+    foto TEXT,
+    localizacao TEXT,
+    dataInicio TIMESTAMP NOT NULL,
+    dataFim TIMESTAMP,
+    isPublic BOOLEAN NOT NULL DEFAULT FALSE,
+    idOrganizacao UUID NOT NULL REFERENCES Organizacao (idOrganizacao) ON DELETE CASCADE,
+
+    CONSTRAINT data_fim_check CHECK (dataFim IS NULL OR dataInicio < dataFim),
+    CONSTRAINT data_inicio_check CHECK (dataInicio > current_timestamp)
 );
 
-CREATE TABLE VotoComentario (
-    idUtilizador INT,
-    idComentario INT,
-    isUp BOOLEAN,
-    PRIMARY KEY (idUtilizador, idComentario),
-    FOREIGN KEY (idUtilizador) REFERENCES Cliente (idUtilizador),
-    FOREIGN KEY (idComentario) REFERENCES Comentario (idComentario)
+DROP TABLE IF EXISTS Participante CASCADE;
+CREATE TABLE Participante (
+    idUtilizador UUID REFERENCES Cliente (idUtilizador) ON DELETE CASCADE,
+    idEvento UUID REFERENCES Evento (idEvento) ON DELETE CASCADE,
+    PRIMARY KEY (idUtilizador, idEvento)
 );
 
-CREATE TABLE Ficheiro (
-    idFicheiro SERIAL PRIMARY KEY,
-    idComentario INT,
-    caminho VARCHAR(255),
-    nome VARCHAR(255),
-    tipo VARCHAR(255),
-    data DATE,
-    FOREIGN KEY (idComentario) REFERENCES Comentario (idComentario)
+DROP TABLE IF EXISTS Organizador CASCADE;
+CREATE TABLE Organizador (
+    idUtilizador UUID REFERENCES Cliente (idUtilizador) ON DELETE CASCADE,
+    idOrganizacao UUID REFERENCES Organizacao (idOrganizacao) ON DELETE CASCADE,
+    PRIMARY KEY (idUtilizador, idOrganizacao)
 );
 
-
-CREATE TABLE NotificacaoUtilizador (
-    idNotificacao SERIAL REFERENCES Notificacao (idNotificacao) ON UPDATE CASCADE ON DELETE CASCADE,
-    idEvento SERIAL NOT NULL REFERENCES Evento (idEvento) ON UPDATE CASCADE ON DELETE CASCADE,
-    PRIMARY KEY (idNotificacao, idEvento)
+DROP TABLE IF EXISTS PedidoRegisto CASCADE;
+CREATE TABLE PedidoRegisto (
+    idPedidoRegisto UUID PRIMARY KEY,
+    idUtilizador UUID NOT NULL REFERENCES Cliente (idUtilizador) ON DELETE CASCADE,
+    idOrganizacao UUID NOT NULL REFERENCES Organizacao (idOrganizacao) ON DELETE CASCADE,
+    data TIMESTAMP NOT NULL DEFAULT current_timestamp 
 );
 
-
-CREATE TABLE ClienteOrganizacao (
-    idUtilizador INT,
-    idOrganizacao INT,
-    PRIMARY KEY (idUtilizador, idOrganizacao),
-    FOREIGN KEY (idUtilizador) REFERENCES Cliente (idUtilizador),
-    FOREIGN KEY (idOrganizacao) REFERENCES Organizacao (idOrganizacao)
-);
-
-CREATE TABLE ClienteEvento (
-    idUtilizador INT,
-    idEvento INT,
-    PRIMARY KEY (idUtilizador, idEvento),
-    FOREIGN KEY (idUtilizador) REFERENCES Cliente (idUtilizador),
-    FOREIGN KEY (idEvento) REFERENCES Evento (idEvento)
-);
-
+DROP TABLE IF EXISTS Tag CASCADE;
 CREATE TABLE Tag (
-    idTag SERIAL PRIMARY KEY,
-    nome VARCHAR(255)
+    idTag UUID PRIMARY KEY,
+    nome TEXT NOT NULL UNIQUE
 );
 
+DROP TABLE IF EXISTS TagEvento CASCADE;
 CREATE TABLE TagEvento (
-    idEvento INT,
-    idTag INT,
-    PRIMARY KEY (idEvento, idTag),
-    FOREIGN KEY (idEvento) REFERENCES Evento (idEvento),
-    FOREIGN KEY (idTag) REFERENCES Tag (idTag)
+    idTag UUID REFERENCES Tag (idTag) ON DELETE CASCADE,
+    idEvento UUID REFERENCES Evento (idEvento) ON DELETE CASCADE,
+    PRIMARY KEY (idTag, idEvento)
 );
 
-CREATE TABLE motivosDenuncia (
-    idMotivoDenuncia SERIAL PRIMARY KEY
+DROP TABLE IF EXISTS Comentario CASCADE;
+CREATE TABLE Comentario (
+    idComentario UUID PRIMARY KEY,
+    idUtilizador UUID NOT NULL REFERENCES Cliente (idUtilizador) ON DELETE CASCADE,
+    texto TEXT NOT NULL,
+    data TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    balancoVotos INT NOT NULL DEFAULT 0,
+    idEvento UUID NOT NULL REFERENCES Evento (idEvento)
 );
 
-CREATE TABLE Denuncia (
-    idDenuncia SERIAL PRIMARY KEY,
-    resolvido BOOLEAN,
-    data DATE,
-    idMotivoDenuncia INT NOT NULL,
-    FOREIGN KEY (idMotivoDenuncia) REFERENCES motivosDenuncia (idMotivoDenuncia)
+DROP TABLE IF EXISTS VotoComentario CASCADE;
+CREATE TABLE VotoComentario (
+    idUtilizador UUID REFERENCES Cliente (idUtilizador) ON DELETE CASCADE,
+    idComentario UUID REFERENCES Comentario (idComentario) ON DELETE CASCADE,
+    isUp BOOLEAN NOT NULL,
+    PRIMARY KEY (idUtilizador, idComentario)
 );
 
+DROP TABLE IF EXISTS Ficheiro CASCADE;
+CREATE TABLE Ficheiro (
+    idFicheiro UUID PRIMARY KEY,
+    idComentario UUID NOT NULL REFERENCES Comentario (idComentario) ON DELETE CASCADE,
+    caminho TEXT NOT NULL,
+    nome TEXT NOT NULL,
+    tipo TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS MotivoDenunciaEvento CASCADE;
+CREATE TABLE MotivoDenunciaEvento (
+    idMotivoDenunciaEvento UUID PRIMARY KEY,
+    texto TEXT NOT NULL UNIQUE
+);
+
+DROP TABLE IF EXISTS DenunciaEvento CASCADE;
 CREATE TABLE DenunciaEvento (
-    idDenuncia INT,
-    idEvento INT NOT NULL,
-    FOREIGN KEY (idDenuncia) REFERENCES Denuncia (idDenuncia),
-    FOREIGN KEY (idEvento) REFERENCES Evento (idEvento)
+    idDenunciaEvento UUID PRIMARY KEY,
+    idEvento UUID NOT NULL REFERENCES Evento (idEvento) ON DELETE CASCADE,
+    resolvido BOOLEAN NOT NULL DEFAULT FALSE,
+    data TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    idMotivoDenunciaEvento UUID NOT NULL REFERENCES MotivoDenunciaEvento (idMotivoDenunciaEvento) ON DELETE CASCADE
 );
 
+DROP TABLE IF EXISTS MotivoDenunciaComentario CASCADE;
+CREATE TABLE MotivoDenunciaComentario (
+    idMotivoDenunciaComentario UUID PRIMARY KEY,
+    texto TEXT NOT NULL UNIQUE
+);
+
+DROP TABLE IF EXISTS DenunciaComentario CASCADE;
 CREATE TABLE DenunciaComentario (
-    idDenuncia INT,
-    idComentario INT NOT NULL,
-    FOREIGN KEY (idDenuncia) REFERENCES Denuncia (idDenuncia),
-    FOREIGN KEY (idComentario) REFERENCES Comentario (idComentario)
+    idDenunciaCOmentario UUID,
+    idComentario UUID NOT NULL REFERENCES Comentario (idComentario) ON DELETE CASCADE,
+    resolvido BOOLEAN NOT NULL DEFAULT FALSE,
+    data TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    idMotivoDenunciaComentario UUID NOT NULL REFERENCES MotivoDenunciaComentario (idMotivoDenunciaComentario) ON DELETE CASCADE
 );
 
+DROP TABLE IF EXISTS Notificacao CASCADE;
+CREATE TABLE Notificacao (
+    idNotificacao UUID PRIMARY KEY,
+    texto TEXT NOT NULL,
+    data TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    url TEXT NOT NULL,
+    visto BOOLEAN NOT NULL DEFAULT FALSE,
+    utilizadorNotificado UUID NOT NULL REFERENCES Utilizador (idUtilizador) ON DELETE CASCADE
+);
 
+DROP TABLE IF EXISTS NotificacaoUtilizador CASCADE;
+CREATE TABLE NotificacaoUtilizador (
+    idNotificacao UUID PRIMARY KEY REFERENCES Notificacao (idNotificacao) ON DELETE CASCADE,
+    idUtilizador UUID NOT NULL REFERENCES Utilizador (idUtilizador) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS NotificacaoEvento CASCADE;
+CREATE TABLE NotificacaoEvento (
+    idNotificacao UUID  PRIMARY KEY REFERENCES Notificacao (idNotificacao) ON DELETE CASCADE,
+    idEvento UUID NOT NULL REFERENCES Evento (idEvento) ON DELETE CASCADE
+);
