835759da5957a238708bcf68f3b2a0d54ec54f98
Francisco da Ana <franciscopdana@gmail.com>
2023-11-16 16:40:54 +0000

    clean thingy stuff

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,13 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Auth;
 
-use App\Models\Card;
-
 class HomeController extends Controller
 {
-    /**
-     * Show the card for a given id.
-     */
     public function show(string $id): View
     {
         return view('pages.home');
