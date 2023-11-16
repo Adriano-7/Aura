d1dbec64340563171641b5d423a6395c66f97656
Francisco da Ana <franciscopdana@gmail.com>
2023-11-16 16:27:01 +0000

    add href from login to home

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,7 @@
       <div class="row">
         <div class="col-sm-6 text-black">
           <div class="px-5 ms-xl-4">
-            <img src="{{asset('images/AuraLogo.svg')}}" alt="Logo" style="width: 7rem;" class="pt-5 mt-xl-4">
+            <a href="/home"><img src="{{asset('images/AuraLogo.svg')}}" alt="Logo" style="width: 7rem;" class="pt-5 mt-xl-4"></a>
           </div>
 
           <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-xl-n5">
