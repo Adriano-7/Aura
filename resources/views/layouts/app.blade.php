f919a9b0c9d07d5ba2fc9cd7fded3a1aa6137efd
Adriano Machado <adriano.alexandre.machado1@gmail.com>
2023-11-16 10:22:06 +0000

    Changed the way we import bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Styles -->
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" crossorigin="anonymous">
 
     </head>
     <body>
