4709ae1bdbcb32bc5e797863c340a1834f844504
Evans <jpjoao24@hotmail.com>
2023-11-22 17:12:52 +0000

    Fixed image ratios

--- resources/views/widgets/eventTable.blade.php
+++ resources/views/widgets/eventTable.blade.php
@@ -1,5 +1,5 @@
 
-<div class="container-fluid px-sm-0 px-lg-3 mt-5">
+<div class="container-fluid px-sm-0  mt-5">
     <div class="row justify-content-center">
         <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-7">
             <div class="row options">
@@ -19,9 +19,13 @@
             @foreach($events as $event)
                 <div class="card mb-4 shadow-sm clickable-card" data-href="{{ route('events', ['id' => $event->id]) }}" style="border-radius: 15px;">
                     <div class="row ">
-                        <div class="col-3 col-md-2 p-2 text-center d-sm-block">             
-                            <img src="{{ asset('storage/eventos/' . $event->photo) }}" class="card-img " alt="Event Photo"></div>
-                        <div class="col-5 col-md-7 p-1 card-container d-flex align-items-center">
+                        <div class="col-4 col-md-3 col-lg-3 col-xl-2 p-2 text-center d-sm-block">             
+                        <img class=" card-img-aura img-fluid" src="{{ asset('storage/eventos/' . $event->photo) }}"
+                                  alt="Card image cap" style="object-fit: cover;">
+                        </div>
+        
+                        
+                        <div class="col-4 col-md-6 p-1 card-container d-flex align-items-center">
                             <div class="card-body">
                                 <h5 class="card-title d-flex align-items-center">{{ $event->name }}&nbsp
                                 @if($header == 'Organizo')
