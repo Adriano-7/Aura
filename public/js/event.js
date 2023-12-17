e3711148cb8807fdb24c4be32d8f912e317ffa2b
Francisco da Ana <franciscopdana@gmail.com>
2023-12-17 20:01:06 +0000

    changed votes js still not sure if its working good

--- public/js/event.js
+++ public/js/event.js
@@ -1,5 +1,194 @@
 let csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
+/*
+
+~ unfilled up arrow
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffffff" class="bi bi-arrow-up-circle L0" viewBox="0 0 16 16" style="cursor: pointer; margin-right:0.5em" id="{{'upIcon-' . $comment->id}}">
+    <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
+</svg>
+
+~ unfilled down arrow
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffffff" class="bi bi-arrow-down-circle D0" viewBox="0 0 16 16" style="cursor: pointer; margin-right:0.5em" id="{{'downIcon-' . $comment->id}}">
+    <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
+</svg>
+
+~ filled up arrow
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffffff" class="bi bi-arrow-up-circle-fill L1" viewBox="0 0 16 16" style="cursor: pointer; margin-right:0.5em" id="{{'upIcon-' . $comment->id}}">
+    <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
+</svg>
+
+~ filled down arrow
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffffff" class="bi bi-arrow-down-circle-fill D-1" viewBox="0 0 16 16" style="cursor: pointer; margin-right:0.5em" id="{{'downIcon-' . $comment->id}}">
+    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
+</svg>
+
+*/
+
+document.addEventListener('click', function(event){
+    if(event.target.matches('.L0')){
+        console.log("You clicked in L0");
+
+        let commentId = event.target.id.split('-')[1];
+        fetch(new URL(`api/comentario/${commentId}/up`,  window.location.origin), {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'X-CSRF-TOKEN': csrfToken
+            },
+        }).then(res => {
+                if (res.ok) {
+                    event.target.classList.remove("L0");
+                    event.target.classList.add("L1");
+                    event.target.innerHTML = '<path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>';
+                    let otherBtnId = 'downIcon-' + commentId;
+                    let otherBtn = document.getElementById(otherBtnId);
+                    otherBtn.classList.remove("D0");
+                    otherBtn.classList.add("D1");
+                    let voteSpan = Array.from(event.target.parentNode.children).find(el => el.className === 'comment-votes');
+                    let voteBalance = parseInt(voteSpan.textContent);
+                    voteSpan.textContent = voteBalance + 1;
+                }
+            })
+        .catch(err => console.log(err));
+    }
+
+    if(event.target.matches('.D0')){        
+        console.log("You clicked in D0");
+        
+        let commentId = event.target.id.split('-')[1];
+        fetch(new URL(`api/comentario/${commentId}/down`,  window.location.origin), {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'X-CSRF-TOKEN': csrfToken
+            },
+        }).then(res => {
+                if (res.ok) {
+                    event.target.classList.remove("D0");
+                    event.target.classList.add("D-1");
+                    event.target.innerHTML = '<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>';
+                    let otherBtnId = 'upIcon-' + commentId;
+                    let otherBtn = document.getElementById(otherBtnId);
+                    otherBtn.classList.remove("L0");
+                    otherBtn.classList.add("L-1");
+                    let voteSpan = Array.from(event.target.parentNode.children).find(el => el.className === 'comment-votes');
+                    let voteBalance = parseInt(voteSpan.textContent);
+                    voteSpan.textContent = voteBalance - 1;
+                }
+            })
+        .catch(err => console.log(err));
+    }
+
+    if(event.target.matches('.L1')){
+        console.log("You clicked in L1");
+        
+        let commentId = event.target.id.split('-')[1];
+        fetch(new URL(`api/comentario/${commentId}/unvote`,  window.location.origin), {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json',
+                'X-CSRF-TOKEN': csrfToken
+            },
+        }).then(res => {
+                if (res.ok) {
+                    event.target.classList.remove("L1");
+                    event.target.classList.add("L0");
+                    event.target.innerHTML = '<path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>';
+                    let otherBtnId = 'downIcon-' + commentId;
+                    let otherBtn = document.getElementById(otherBtnId);
+                    otherBtn.classList.remove("D1");
+                    otherBtn.classList.add("D0");
+                    let voteSpan = Array.from(event.target.parentNode.children).find(el => el.className === 'comment-votes');
+                    let voteBalance = parseInt(voteSpan.textContent);
+                    voteSpan.textContent = voteBalance - 1;
+                }
+            })
+        .catch(err => console.log(err));
+    }
+
+    if(event.target.matches('.D1')){
+        console.log("You clicked in D1");
+        
+        let commentId = event.target.id.split('-')[1];
+        fetch(new URL(`api/comentario/${commentId}/unvote`,  window.location.origin), {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json',
+                'X-CSRF-TOKEN': csrfToken
+            },
+        }).then(res => {
+                if (res.ok) {
+                    event.target.classList.remove("D1");
+                    event.target.classList.add("D0");
+                    let otherBtnId = 'upIcon-' + commentId;
+                    let otherBtn = document.getElementById(otherBtnId);
+                    otherBtn.classList.remove("L1");
+                    otherBtn.classList.add("L0");
+                    otherBtn.innerHTML = '<path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>';
+                    let voteSpan = Array.from(event.target.parentNode.children).find(el => el.className === 'comment-votes');
+                    let voteBalance = parseInt(voteSpan.textContent);
+                    voteSpan.textContent = voteBalance - 1;
+                }
+            })
+        .catch(err => console.log(err));
+    }
+
+    if(event.target.matches('.L-1')){
+        console.log("You clicked in L-1");
+        
+        let commentId = event.target.id.split('-')[1];
+        fetch(new URL(`api/comentario/${commentId}/unvote`,  window.location.origin), {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json',
+                'X-CSRF-TOKEN': csrfToken
+            },
+        }).then(res => {
+                if (res.ok) {
+                    event.target.classList.remove("L-1");
+                    event.target.classList.add("L0");
+                    let otherBtnId = 'downIcon-' + commentId;
+                    let otherBtn = document.getElementById(otherBtnId);
+                    otherBtn.classList.remove("D-1");
+                    otherBtn.classList.add("D0");
+                    otherBtn.innerHTML = '<path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>';
+                    let voteSpan = Array.from(event.target.parentNode.children).find(el => el.className === 'comment-votes');
+                    let voteBalance = parseInt(voteSpan.textContent);
+                    voteSpan.textContent = voteBalance + 1;
+                }
+            })
+        .catch(err => console.log(err));
+    }
+
+    if(event.target.matches('.D-1')){        
+        console.log("You clicked in D-1");
+        
+        let commentId = event.target.id.split('-')[1];
+        fetch(new URL(`api/comentario/${commentId}/unvote`,  window.location.origin), {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json',
+                'X-CSRF-TOKEN': csrfToken
+            },
+        }).then(res => {
+                if (res.ok) {
+                    event.target.classList.remove("D-1");
+                    event.target.classList.add("D0");
+                    event.target.innerHTML = '<path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>';
+                    let otherBtnId = 'upIcon-' + commentId;
+                    let otherBtn = document.getElementById(otherBtnId);
+                    otherBtn.classList.remove("L-1");
+                    otherBtn.classList.add("L0");
+                    let voteSpan = Array.from(event.target.parentNode.children).find(el => el.className === 'comment-votes');
+                    let voteBalance = parseInt(voteSpan.textContent);
+                    voteSpan.textContent = voteBalance + 1;
+                }
+            })
+        .catch(err => console.log(err));
+    }
+});
+
+/*
 $(document).on('click', '.L0', function(){
     let commentId = $(this).attr('id').split('-')[1];
 
@@ -143,22 +332,7 @@ $(document).on('click', '.D-1', function(){
         })
     .catch(err => console.log(err));
 });
-
-const deleteCommentButtons = document.querySelectorAll('.delete-comment-btn');
-
-deleteCommentButtons.forEach(button => {
-    button.addEventListener('click', async e => {
-        deleteComment(button);
-    });
-});
-
-const editCommentButtons = document.querySelectorAll('.edit-comment-btn');
-
-editCommentButtons.forEach(button => {
-    button.addEventListener('click', async e => {
-        activateEditComment(button);
-    });
-});
+*/
 
 document.addEventListener('submit', function(e){
     if(e.target.matches('.edit-comment-form')) {
@@ -167,6 +341,20 @@ document.addEventListener('submit', function(e){
     if(e.target.matches('#add-comment-form')) {
         addComment(e);
     }
+});
+
+const deleteCommentButtons = document.querySelectorAll('.delete-comment-btn');
+deleteCommentButtons.forEach(button => {
+    button.addEventListener('click', async e => {
+        deleteComment(button);
+    });
+});
+
+const editCommentButtons = document.querySelectorAll('.edit-comment-btn');
+editCommentButtons.forEach(button => {
+    button.addEventListener('click', async e => {
+        activateEditComment(button);
+    });
 });
 
 function updateComment(e){
@@ -336,9 +524,7 @@ function deleteComment(button){
         },
     }).then(res => {
             if (res.ok) {
-                // remove comment row
                 commentRow.parentNode.removeChild(commentRow);
-                // update comments count
                 let commentCountElement = document.querySelector('#comments h2');
                 let commentCount = commentCountElement.textContent.split('(')[1].split(')')[0];
                 commentCount = parseInt(commentCount);
